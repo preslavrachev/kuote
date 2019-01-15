8fa26dbaa8d76d9194317692fcfb5322d456041d
Preslav Rachev <rachev.preslav@gmail.com>
2019-01-15 10:18:54 +0100

    Extends the User object with a Profile:
    Allows for adding custom properties associated with a particular user
    
    based on:
    - https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-user-model.html
    - https://simpleisbetterthancomplex.com/tutorial/2016/11/23/how-to-add-user-profile-to-django-admin.html
    - https://docs.djangoproject.com/en/dev/topics/auth/customizing/#extending-the-existing-user-model

--- kuotes/models.py
+++ kuotes/models.py
@@ -1,4 +1,23 @@
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from djongo import models
+
+
+class TwitterAccount(models.Model):
+    avatar_url = models.URLField(verbose_name='Avatar URL', blank=True)
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    twitter_account = models.EmbeddedModelField(model_container=TwitterAccount)
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+    instance.profile.save()
 
 
 class Source(models.Model):
