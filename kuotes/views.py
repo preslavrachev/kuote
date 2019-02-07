2346ed4d19315fe92ff5eea0538d764e6ade3112
Preslav Rachev <rachev.preslav@gmail.com>
2019-02-07 07:36:13 +0100

    Displays a proper source domain on the card

--- kuotes/views.py
+++ kuotes/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from urllib import parse
 
 from kuotes.models import Kuote
 
 
+def _enrich(quote):
+    quote.source.domain = parse.urlparse(quote.source.url).netloc
+    return quote
+
+
 def index(request):
     context = {
-        'recent_kuotes': Kuote.objects.all()[:5]
+        'recent_kuotes': list(map(_enrich, Kuote.objects.all()[:5]))
     }
     return render(request, 'index.html', context)
