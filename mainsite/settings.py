4033e3aba604c777c4f2c4b418b0896b018a0d97
Preslav Rachev <rachev.preslav@gmail.com>
2019-02-08 07:41:22 +0100

    ALLOWED_HOSTS setup for heroku

--- mainsite/settings.py
+++ mainsite/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0', 'kuote-app.herokuapp.com']
 
 # Application definition
 
